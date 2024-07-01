a18648816d4c689b07e021a72e0955b31bf58158
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-07-01 19:56:27 -0300

    Add files via upload

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+
+            Console.WriteLine($"Tabuada do número {numero}:");
+
+            // Loop para gerar e exibir a tabuada do número 5
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+        }
+    }
+ 
