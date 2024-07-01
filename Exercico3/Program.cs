ae043b899e194d6948760b023981fd2d9d002cf5
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-07-01 19:53:56 -0300

    Add files via upload

---
+++ Exercico3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercico3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexo;
+
+            // Solicita ao usuário para entrar com o sexo, garantindo que seja "F" ou "M"
+            while (true)
+            {
+                Console.Write("Digite o sexo (F para feminino, M para masculino): ");
+                sexo = Console.ReadLine().ToUpper();
+
+                if (sexo == "F" || sexo == "M")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, digite 'F' para feminino ou 'M' para masculino.");
+                }
+            }
+
+            // Exibe o sexo válido inserido
+            Console.WriteLine($"Sexo inserido: {sexo}");
+        }
+    }
+}
---
+++ Exercico3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Exercico3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercico3")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("10e4e687-a5d8-48cb-9d8c-03a34394e47e")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
