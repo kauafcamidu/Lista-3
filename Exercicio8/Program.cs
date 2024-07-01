374e610199a194a8e6ca34bc4a78dde64fb4a5d3
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-07-01 20:03:20 -0300

    Add files via upload

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            // Loop para somar os números de 1 a 100
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += i;
+            }
+
+            // Exibe a soma total
+            Console.WriteLine($"A soma dos números inteiros positivos de 1 a 100 é: {soma}");
+        }
+    }
+}
+    
+
