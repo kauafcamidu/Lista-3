fef8437202a4a9214672fb57850fa53ae2ade4a2
kauafcamidu <168488826+kauafcamidu@users.noreply.github.com>
2024-07-01 19:58:07 -0300

    Add files via upload

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+
+            // Solicita ao usuário para entrar com um valor positivo
+            while (true)
+            {
+                Console.Write("Digite um valor positivo: ");
+                valor = Convert.ToDouble(Console.ReadLine());
+
+                if (valor > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido. Por favor, digite um valor positivo.");
+                }
+            }
+
+            // Exibe a tabuada do valor inserido no intervalo de um a dez
+            Console.WriteLine($"Tabuada do número {valor}:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{valor} x {i} = {valor * i}");
+            }
+        }
+    }
+}
+    
+
